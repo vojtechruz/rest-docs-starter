56853c7f6048981deda3bddbfabd2b99423d3fbb
Vojtěch Růžička <vojtech.ruz@gmail.com>
2018-02-16 08:28:22 +0100

    Reverted springfox specific changes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# springfox-example
-Example project implementing Swagger rest documentation using SpringFox.
+# Rest Docs Starter
+Starter repository with a simple REST controller used as a starting point for trying REST API documentation.
--- src/main/java/com/vojtechruzicka/springfoxexample/controllers/PersonController.java
+++ src/main/java/com/vojtechruzicka/springfoxexample/controllers/PersonController.java
@@ -2,46 +2,34 @@ package com.vojtechruzicka.springfoxexample.controllers;
 
 import com.vojtechruzicka.springfoxexample.domain.Person;
 import com.vojtechruzicka.springfoxexample.services.PersonService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v2/persons/")
-@Api(description = "Set of endpoints for Creating, Retrieving, Updating and Deleting of Persons.")
 public class PersonController {
 
     private PersonService personService;
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    @ApiOperation("${personcontroller.getallpersons}")
     public List<Person> getAllPersons() {
         return personService.getAllPersons();
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/{id}", produces = "application/json")
-    @ApiOperation("${personcontroller.getpersonbyid}")
-    public Person getPersonById(@ApiParam("Id of the person to be obtained. Cannot be empty.")
-                                    @PathVariable int id) {
+    public Person getPersonById(@PathVariable int id) {
         return personService.getPersonById(id);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, path = "/{id}")
-    @ApiOperation("${personcontroller.deleteperson}")
-    public void deletePerson(@ApiParam("Id of the person to be deleted. Cannot be empty.")
-                                 @PathVariable int id) {
+    public void deletePerson(@PathVariable int id) {
         personService.deletePerson(id);
     }
 
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    @ApiOperation("${personcontroller.createperson}")
-    public Person createPerson(@ApiParam("Person information for a new person to be created.")
-                                   @RequestBody Person person) {
+    public Person createPerson(@RequestBody Person person) {
         return personService.createPerson(person);
     }
 
